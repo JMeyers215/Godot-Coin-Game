49f710fb22e64f44f09c30cfa6e8cbe2c433e27d
James Meyers <jmmeyers215@gmail.com>
2023-10-08 12:12:29 -0600

    Finalizing the coin game for the Godot lesson
    Finalizing the touches on the player script, coin script, and linking all of the scenes together to create a simple game

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
+# player move speed variable
+var move_speed : float = 200.0
 
 func _physics_process(delta):
+	#sets initial velocity to 0
 	velocity.x = 0
 	velocity.y = 0
 	
+	# all input directions
 	if Input.is_key_pressed(KEY_LEFT):
 		velocity.x -= 1
 	if Input.is_key_pressed(KEY_RIGHT):
@@ -13,4 +17,5 @@ func _physics_process(delta):
 	if Input.is_key_pressed(KEY_DOWN):
 		velocity.y += 1
 		
+	velocity *= move_speed
 	move_and_slide()
